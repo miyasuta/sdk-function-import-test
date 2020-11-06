9a03a054e39230c6feba45b7c757b775ccb17757
Mio Yasutake <mio.fujita.01@gmail.com>
2020-11-06 20:22:28 +0900

    modify message

--- data-consumer/src/get-delivery/get-delivery.controller.ts
+++ data-consumer/src/get-delivery/get-delivery.controller.ts
@@ -1,22 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
-import { functionImports } from 'odata-client/catalog-service';
+    import { Controller, Get } from '@nestjs/common';
+    import { functionImports } from 'odata-client/catalog-service';
 
 
-@Controller('get-delivery')
-export class GetDeliveryController {
-    @Get()
-    getDeliveryInfo() {
-        return new Promise((resolve, reject) => {
-            functionImports.getDeliveryDate({})
-            .execute({
-                url: 'http://localhost:4004'                
+    @Controller('get-delivery')
+    export class GetDeliveryController {
+        @Get()
+        getDeliveryInfo() {
+            return new Promise((resolve, reject) => {
+                functionImports.getDeliveryDate({})
+                .execute({
+                    url: 'http://localhost:4004'                
+                })
+                .then(res => {
+                    resolve('Function call successful: ' + JSON.stringify(res))
+                })
+                .catch(error => {
+                    resolve('Error occurred: ' + JSON.stringify(error))
+                })
             })
-            .then(res => {
-                resolve('Function call successful: ' + JSON.stringify(res))
-            })
-            .catch(error => {
-                resolve('Error ocurred: ' + JSON.stringify(error))
-            })
-        })
+        }
     }
-}
